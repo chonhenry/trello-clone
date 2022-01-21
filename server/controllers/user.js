a396aa63d51f070d8f9f53e083911a198f0f8692
Henry Chon 2 <chonhenry@outlook.com>
2022-01-21 16:02:56 -0500

    signup

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,17 +1,40 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
 import UserModel from "../models/user.js";
 
+const secret = "jwtsecret";
+
 export const signin = (req, res) => {
-  res.json({ msg: "signin route" });
+  const { email, password } = req.body;
 };
 
 export const signup = async (req, res) => {
   const { email, name, password } = req.body;
 
   try {
-    res.json({ msg: "signup route", email, name, password });
+    const existingUser = await UserModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "There is already an account associated with this email.",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    console.log(hashedPassword);
+
+    const newUser = await UserModel.create({
+      email,
+      name,
+      password: hashedPassword,
+      boards: [],
+    });
+
+    const token = jwt.sign({ email: newUser.email, id: newUser._id }, secret, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ newUser, token });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
