6e3268f9e6f4966751538476e7109783fffdd905
Henry Chon 2 <chonhenry@outlook.com>
2022-01-26 07:50:59 -0500

    create board route

--- server/controllers/board.js
+++ server/controllers/board.js
@@ -1,9 +1,19 @@
 import BoardModel from "../models/board.js";
 
 export const createBoard = async (req, res) => {
-  res.json({
-    message: "create board",
-    title: req.body.title,
-    userId: req.userId,
-  });
+  const { title } = req.body;
+
+  if (!title) return res.status(400).json({ message: "Title missing" });
+
+  try {
+    const newBoard = await BoardModel.create({
+      title,
+      userId: req.userId,
+    });
+
+    res.status(200).json({ newBoard });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
