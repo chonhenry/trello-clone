d7930226c55ccb0f5b022e3f603a19b0e7504833
Henry Chon 2 <chonhenry@outlook.com>
2022-02-11 23:04:35 -0500

    changed url

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://trello-clone-project.herokuapp.com/",
+  baseURL: "https://trello-clone-project.herokuapp.com",
 });
 
 interface SignupForm {
